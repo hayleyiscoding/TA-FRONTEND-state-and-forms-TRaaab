4cf1fe3eff0c81e49bf34eb78349d24bae483a8a
Hayley Wood <onlinecowork@gmail.com>
2022-04-04 18:16:22 +1000

    accordion React

---
+++ accordion/src/App.js
@@ -0,0 +1,14 @@
+import Accordion from "./components/Accordion";
+
+function App() {
+  return (
+    <>
+      <div className='container padding'>
+        <h1>Closed State</h1>
+      </div>
+      <Accordion />
+    </>
+  );
+}
+
+export default App;
---
+++ accordion/src/components/Accordion.js
@@ -0,0 +1,12 @@
+function Accordion() {
+  return (
+    <div className='accordion-container'>
+      <ul>
+        <li>Javascript</li>
+        <li>React</li>
+        <li>CSS</li>
+      </ul>
+    </div>
+  );
+}
+export default Accordion;
